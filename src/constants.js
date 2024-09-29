ee3fc3dd7c718143ad9a88ae3682cba550bc9431
Mirza Yousuf Zulfiqar Baig <myzbaig@mun.ca>
2024-09-29 19:17:53 -0230

    updated css

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const scaleFactor = 4;
 export const dialogueData = {
     pc: `This is my PC. I work mostly in JS/TS these days.
       I've made a couple of websites and games in that language. I also like React frameworks and Python. Anyway regardless of the language, I just like programming.
-      Here is my +<a href="https://github.com/akurtle" target="_blank">Github</a>}!`,
+      Here is my <a href="https://github.com/akurtle" target="_blank">Github</a>}!`,
     "cs-degree": `I am currently starting my fourth year of UNI, I have a GPA of 3.68 and I'm majoring in Computer Science.`,
     "sofa-table": `That's my sofa. I like to relax here and watch YouTube. I love watching cat videos in my free time`,
     tv: `That's my TV. I've been watching tech youtubers a lot recently like :
@@ -11,7 +11,7 @@ export const dialogueData = {
     <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
     bed: `This where I sleep. Great ideas comes when I'm lying on my bed. Resting is important keeps my ideas fresh and logic on point!`,
     resume: `This is my desk and on it is my resume. <a href="https://github.com/akurtle" target="_blank">Check it out?</a>
-    Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+    Contact me at myzbaig@mun.ca if you have any interesting job opportunities!`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. I have created multiple websites, Machine learning projects and much more!`,
     library: `I love taking cs courses on various topics from time to time apart from uni, I have done the following courses`,
